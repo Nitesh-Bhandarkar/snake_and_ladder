fc11289dc786ea85cb05d5429dd270b5d4744099
Nitesh Bhandarkar <niteshbhandarkar@ITs-MacBook-Pro.local>
2026-01-02 11:48:28 +0530

    Init set up

---
+++ src/test/java/com/lld/snake_and_ladder/SnakeAndLadderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lld.snake_and_ladder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnakeAndLadderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
